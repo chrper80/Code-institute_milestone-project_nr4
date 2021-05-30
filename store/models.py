4aaca13a47a27759bcbac9b46d2ffd9194312854
chrper80 <chrper80@gmail.com>
2021-05-30 10:36:09 +0000

    Added 2 models (Category and Product). Added a 'store' app. In the store app there is so far an index page that is now displaying 3 categories

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    category_description = models.CharField(max_length=100)
+    category_image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_description = models.CharField(max_length=100)
+    product_price = models.IntegerField()
+    product_image = models.ImageField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Category
 
 
 def index(request):
-    return render(request, 'store/index.html')
+    categories = Category.objects.all()
+
+    context = {
+        "categories": categories
+    }
+    return render(request, 'store/index.html', context)
