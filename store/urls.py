53e79a5f1c30c1f6c2d00bffa60cfb6ed291eeec
chrper80 <chrper80@gmail.com>
2021-05-30 11:54:12 +0000

    The user can now follow a link in the categories page and look at each product individually

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('categories/<category_id>', views.categories, name="categories"),
+    path('ind_products/<product_id>', views.ind_products, name="ind_products"),
 ]
