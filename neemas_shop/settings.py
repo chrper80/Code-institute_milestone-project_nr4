ce53f8739ec5058bf1784fdd2e4ae66258c506a2
chrper80 <chrper80@gmail.com>
2021-06-10 11:30:18 +0000

    deployed to Heroku, removed secret key and changed debug from True

--- neemas_shop/settings.py
+++ neemas_shop/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mjqgdq_l*=ae4f4jje*ku6%wihufnz=k1dqrccy&56%we^1591'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project4.herokuapp.com', 'localhost']
 
